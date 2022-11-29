7dfca2332a273032d92beff35c0f88b67c4d0593
sabahassaad19 <86877185+sabahassaad19@users.noreply.github.com>
2022-11-29 12:03:07 +0200

    Update account_move_line.py

--- second_third_currency/models/account_move_line.py
+++ second_third_currency/models/account_move_line.py
@@ -68,6 +68,32 @@ class AccountMoveLineInherit(models.Model):
 class AccountMove(models.Model):
     _inherit = 'account.move'
     
+    second_amount = fields.Monetary(string='Second Amount', default=0.0, readonly=False, store=True,
+                                    compute="exchange_second_amount")
+    third_amount = fields.Monetary(string='Third Amount', default=0.0, readonly=False, store=True,
+                                   compute="exchange_third_amount")
+    second_currency = fields.Many2one('res.currency', related ="company_id.second_currency")
+    third_currency = fields.Many2one('res.currency', related ="company_id.third_currency")
+    
+    @api.depends('amount_total','amount_residual')
+    def exchange_second_amount(self):
+        for rec in self:
+            second = rec.env['res.currency'].search([('id', '=', rec.company_id.second_currency.id)])
+            if rec.amount_residual:
+                rec.second_amount = rec.currency_id.compute(rec.amount_residual, second)
+            else:
+                rec.second_amount = rec.currency_id.compute(rec.amount_total, second)
+
+
+    @api.depends('amount_total', 'amount_residual')
+    def exchange_third_amount(self):
+        for rec in self:
+            third = rec.env['res.currency'].search([('id', '=', rec.company_id.third_currency.id)])
+            if rec.amount_residual:
+                rec.third_amount = rec.currency_id.compute(rec.amount_residual, third)
+            else:
+                rec.third_amount = rec.currency_id.compute(rec.amount_total, third)
+    
     
     @api.onchange('invoice_line_ids')
     def get_debit_credit(self):
