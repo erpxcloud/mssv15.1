f8115e2c14e99cd8df59fe180df84f9ca25b0d1c
ghinaa-rabah <100515640+ghinaa-rabah@users.noreply.github.com>
2022-11-30 13:26:24 +0200

    Update register_payment_payroll.py

--- payroll_register_payment/models/register_payment_payroll.py
+++ payroll_register_payment/models/register_payment_payroll.py
@@ -1,12 +1,12 @@
 from odoo import fields, models, api
 
-    
+
 class AccountPayment(models.Model):
     _inherit = 'account.payment'
 
     payroll_slip_id = fields.Many2one('hr.payslip')
     visible_payroll_slip_id = fields.Boolean(string='Visibility', compute='compute_visibility', store='True')
-    
+
     def compute_visibility(self):
         for payment in self:
             self.visible_payroll_slip_id = False
@@ -17,25 +17,25 @@ class AccountPayment(models.Model):
 class RegisterPaymentPayslips(models.Model):
     _inherit = 'hr.payslip'
 
-#     payment_method_id = fields.Many2one('account.payment.method', string='Payment Type', required=True)
+    #     payment_method_id = fields.Many2one('account.payment.method', string='Payment Type', required=True)
 
     def _compute_payment(self):
         payment_obj = self.env['account.payment']
         self.payment_id = payment_obj.search([('payroll_slip_id', '=', self.id)])
 
-#     def _compute_pay_amount(self):
-#         for slip in self:
-#             pay_amount_new = 0.0
-#             for line in slip.line_ids:
-#                 if line.salary_rule_id.code == 'NET':
-#                     payl_amount_new+=line.total
-#             slip.pay_amount = pay_amount_new
-    
+    #     def _compute_pay_amount(self):
+    #         for slip in self:
+    #             pay_amount_new = 0.0
+    #             for line in slip.line_ids:
+    #                 if line.salary_rule_id.code == 'NET':
+    #                     payl_amount_new+=line.total
+    #             slip.pay_amount = pay_amount_new
+
     def _compute_pay_amount(self):
         line_obj = self.env['hr.payslip.line']
         payoff = line_obj.search([('code', 'in', ('NET', 'LIQ')), ('slip_id', '=', self.id)])
         self.pay_amount = payoff.amount
-    
+
     def register_payment(self):
         value_amount = self.pay_amount
         if self.state == 'done':
@@ -43,8 +43,8 @@ class RegisterPaymentPayslips(models.Model):
                 'partner_type': 'supplier',
                 'payment_type': 'outbound',
                 'partner_id': self.employee_id.address_home_id.id,
-#                 'journal_id': self.journal_id.id,
-#                 'payment_method_id': self.payment_method_id.id,
+                #                 'journal_id': self.journal_id.id,
+                #                 'payment_method_id': self.payment_method_id.id,
                 'company_id': self.company_id.id,
                 'amount': value_amount,
                 'currency_id': self.currency_id.id,
@@ -66,9 +66,9 @@ class RegisterPaymentPayslips(models.Model):
                                   default=lambda self: self.env.user.company_id.currency_id)
     pay_amount = fields.Float('Payed amount', compute='_compute_pay_amount', currency_field='currency_id')
     payment_id = fields.Many2one('account.payment', 'Payment', compute="_compute_payment")
-    
+
+
 class AccountPayment(models.Model):
     _inherit = 'hr.contract'
 
-    mobile = fields.char(string='Mobile')    
-    
+    mobile = fields.char(string='Mobile')
