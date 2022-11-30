f3d127c3efb5f152598e85046855cdbb25a312ce
ghinaa-rabah <100515640+ghinaa-rabah@users.noreply.github.com>
2022-11-30 08:37:20 +0200

    Update __manifest__.py

--- expense_register_payment/__manifest__.py
+++ expense_register_payment/__manifest__.py
@@ -3,7 +3,7 @@
     'name': 'Expense Register Payment ',
     'version': '2.0',
 
-    'depends': ['base','account', 'account_reports', 'hr_expense'],
+    'depends': ['base','account', 'account_reports', 'hr_expense', 'second_third_currency'],
     'sequence': "-400",
     'data': [
         'views/expense_payment_payroll.xml',
