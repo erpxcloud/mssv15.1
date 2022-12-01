8f3b5a44f0c182b5409e0a1ef816652380de4453
ghinaa-rabah <100515640+ghinaa-rabah@users.noreply.github.com>
2022-12-01 14:27:49 +0200

    Update register_payment_payroll.py

--- payroll_register_payment/models/register_payment_payroll.py
+++ payroll_register_payment/models/register_payment_payroll.py
@@ -72,8 +72,12 @@ class RegisterPaymentPayslips(models.Model):
 
 class RegisterPaymentBatch(models.Model):
     _inherit = 'hr.payslip.run'
+    
+    def _compute_batch_payment(self):
+        payment_batch_obj = self.env['account.batch.payment']
+        self.batch_payment_id = payment_batch_obj.search([('payroll_batch_id', '=', self.id)])
 
-    batch_payment_id = fields.Many2one('account.batch.payment', string='Batch Payment')
+    batch_payment_id = fields.Many2one('account.batch.payment', string='Batch Payment' compute="_compute_batch_payment")
     is_batch_paid = fields.Boolean(string='Paid Payslips')
 
     def batch_register_payment(self):
@@ -83,19 +87,17 @@ class RegisterPaymentBatch(models.Model):
                 payment_payslip = payslip.register_payment()
             batch_id.is_batch_paid = True
             payments = []
-            for payslip in batch_id.slip_ids:
-                payment = batch_id.env['account.payment'].search([('payroll_slip_id', '=', payslip.id)], limit=1)
-                payments.append(payment)
-                batch_payment = batch_id.env['account.batch.payment'].create({
-                    'journal_id': payment[0].journal_id.id,
-                    'payment_ids': [(4, payment.id, None) for payment in payments],
-                    'payment_method_id': payment[0].payment_method_id.id,
-                    'date': batch_id.date_end,
-                    'batch_type': 'outbound',
-#                     'payroll_batch_id': batch_id.id,
+            payment = batch_id.env['account.payment'].search([('payroll_slip_id', '=', payslip.id)])
+            payments.append(payment)
+            batch_payment = batch_id.env['account.batch.payment'].create({
+                'journal_id': payment[0].journal_id.id,
+                'payment_ids': [(4, payment.id, None) for payment in payments],
+                'payment_method_id': payment[0].payment_method_id.id,
+                'date': batch_id.date_end,
+                'batch_type': 'outbound',
+                'payroll_batch_id': batch_id.id,
                 })
-#                 batch_payment = batch_id.env['account.batch.payment'].create(batch_values)
-#                 batch_payment.action_post
+#                 batch_payment.validate_batch_button()
 
 
 class HrContract(models.Model):
