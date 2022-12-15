e8a9436d4448c4410b64d3ef387d6b1e69ac8b9e
MhmdAliChamass99 <80204667+MhmdAliChamass99@users.noreply.github.com>
2022-12-15 12:47:47 +0200

    Update account_payment_register.py

--- erp_acounting_payment_currency/models/account_payment_register.py
+++ erp_acounting_payment_currency/models/account_payment_register.py
@@ -6,6 +6,9 @@ log = logging.getLogger(__name__)
 class AccountPaymentRegister(models.TransientModel):
     _inherit = 'account.payment.register'
     
+    account_amount=fields.Monetary(string="Payment Amount")
+    account_currency_id=fields.Many2one('res.currency')
+    
     def _prepare_payment_vals(self, invoices):
         values = super(AccountPaymentRegister, self)._prepare_payment_vals(invoices)
         values['account_amount'] = values['amount']
