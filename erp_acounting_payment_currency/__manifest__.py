d902ded4dfcc2c6151581022fc1f24432b7dede5
MhmdAliChamass99 <80204667+MhmdAliChamass99@users.noreply.github.com>
2022-12-15 10:08:04 +0200

    Update __manifest__.py

--- erp_acounting_payment_currency/__manifest__.py
+++ erp_acounting_payment_currency/__manifest__.py
@@ -6,13 +6,12 @@
     'version': '13.0.0.0.4',
     'category': '',
 
-    'depends': ['account_accountant', 'account_batch_payment'],
+    'depends': ['account_accountant', 'account_batch_payment','hr','base'],
 
     'data': [
         'views/account_payment.xml',
         'views/account_payment_wizard.xml',
         'views/account_batch_payment_views.xml',
-        
         'data/updating_journal_items.xml'
     ],
 }
