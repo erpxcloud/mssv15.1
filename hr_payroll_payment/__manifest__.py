dc41d58c79295a4e5226c0dfca1eb0532f1756b2
Hadi Masri <ibrahim.masri1@gmail.com>
2022-12-26 16:07:17 +0200

    Update __manifest__.py

--- hr_payroll_payment/__manifest__.py
+++ hr_payroll_payment/__manifest__.py
@@ -12,6 +12,7 @@
     'depends': [
         'hr_payroll_account',
         'account_batch_payment',
+        'hibou_professional',
     ],
     'data': [
         #'wizard/hr_payroll_register_payment_views.xml',
